4ff3538ebf89630e1512ead905cf1567cfc7f71a
Carlos Perez <ca.perez.barr@gmail.com>
2021-07-07 22:52:22 -0500

    Troubleshooting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,19 @@
 var searchBar = document.getElementById('searchBar');
 var searchBtn = document.getElementById('searchBtn');
 // var searchedCity = searchBar.value;
-// var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=Austin&appid=37e98eb8e58cfe49e4e0561295e9fd4d';
-// var requestUrl = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid=37e98eb8e58cfe49e4e0561295e9fd4d';
-
-// }
 
 
 searchBtn.addEventListener("click", function(event){
     event.preventDefault();
     var searchedCity = searchBar.value;
+    console.log(searchedCity);
     apiRequest(searchedCity);
 })
 
 
 function apiRequest (searchedCity) {
-var completeURL = `'https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=37e98eb8e58cfe49e4e0561295e9fd4d'`;
+var completeURL = "'https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=37e98eb8e58cfe49e4e0561295e9fd4d'";
+console.log(completeURL);
 fetch(completeURL)
 .then(function (response) {
     return response.json();
@@ -24,19 +22,6 @@ fetch(completeURL)
     console.log(data);
     
 })};
-
-
-
-
-
-// function getApi() {
-    
-    
-    
-    // searchBtn.addEventListener("click", function(e){
-        // e.preventDefault();
-        // getApi();
-        // });
     
     
     
